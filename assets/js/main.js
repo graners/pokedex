75da0a79dd9914a85abdd4baadd59df731913dca
Steffanie Graner <graner.steffanie@gmail.com>
2023-03-21 15:09:05 -0300

    Finalização do projeto.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,47 @@
-function convertPokemonToLi(pokemon) {
-  return `
-    <li class="pokemon">
-        <span class="number">#001</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
-            </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-                alt="${pokemon.name}">
-        </div>
-    </li>
-    `;
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
+                </ol>
+                <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+      `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
 }
 
-const pokemonList = document.getElementById("pokemonList");
+loadPokemonItens(offset, limit);
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  // const newList = pokemons.map((pokemon) => convertPokemonToLi(pokemon));  // simplificando fica a linha de baixo
-  // const newList = pokemons.map(convertPokemonToLi);  // para cada item pokemon, vai invocar esta função, sendo desnecessário informar o parâmetro porque seria redundante.
-  // const newHtml = newList.join("");  // unindo a função map com a join para simplificar mais ainda
-  // const newList = pokemons.map(convertPokemonToLi).join('');
-  // pokemonList.innerHTML += newHtml;
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
 
-  // O resultado do código acima simplificado e não verboso:
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+  const qtdRecordNextPage = offset + limit;
 
+  if (qtdRecordNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
 });
